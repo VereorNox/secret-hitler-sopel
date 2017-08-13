9d6224cc8c95f73adad0bad754fdbc9538bfe41f
VereorNox <a.kanaan@live.de>
2017-08-13 20:43:35 +0200

    Update Hitler.py

--- Hitler.py
+++ Hitler.py
@@ -1,8 +1,10 @@
 from sopel.module import commands
 
 players = []
+global ongoing
 ongoing = False
 owner = 'VereorNox'
+finished = 0
 
 
 
@@ -19,7 +21,7 @@ def start(bot, trigger):
 
 
 @commands('join')
-def join(bot, trigger):
+def joinGame(bot, trigger):
     if trigger.nick not in players:
         players.append(trigger.nick)
         bot.say(trigger.nick+" has joined up!")
@@ -30,7 +32,7 @@ def join(bot, trigger):
 @commands('quit')
 def stopGame(bot, trigger):
     if ongoing is False:
-        if trigger.nick in players[0]:
+        if trigger.nick is players[0]:
             bot.say("The game was aborted by the creator "+trigger.nick)
         else:
             bot.say("You didn't start this game, you fool!")
@@ -40,7 +42,7 @@ def stopGame(bot, trigger):
 
 
 @commands('flee')
-def quit(bot, trigger):
+def endGame(bot, trigger):
     if ongoing is False:
         if trigger.nick in players:
             players.remove(trigger.nick)
@@ -49,3 +51,14 @@ def quit(bot, trigger):
             bot.say("You're not signed up you dimwit!")
     else:
         bot.say("The game is still ongoing, you dimwit, you can't leave until Hilter is dead!")
+
+
+def hitlerGame:
+   player_amount = len(players)
+   if player_amount < 5:
+       return
+   elif player_amount > 10:
+       return
+   else:
+       ongoing = True
+       while finished is not 1:
